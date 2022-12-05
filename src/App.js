d1956807e616bc8fc7a8064611cdf238f6a8b58e
pumb3r <mishakvochko@ukr.net>
2022-12-05 11:33:46 +0200

    refactor routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
-import Container from '@mui/material/Container';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Routes, Route } from 'react-router-dom';
+import { useDispatch, } from 'react-redux';
 
-import { Header } from './components';
-import { Home, FullPost, AddPost, Login } from './pages';
-import AboutMuseum from './pages/AboutMuseum';
-import Exposure from './pages/Exposure';
-import { selectIsAuth } from './redux/slice/auth';
+import Router from './router'
 import { fetchAuthMe } from './redux/slice/authActionCreator';
 
 const App = () => {
-  const isAuth = useSelector(selectIsAuth);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchAuthMe());
   });
+
   return (
     <>
-      <Header />
-      <Container maxWidth="lg">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts/:id" element={<FullPost />} />
-          <Route path="/add-post" element={<AddPost />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<AboutMuseum />} />
-          <Route path="/exposure" element={<Exposure />} />
-        </Routes>
-      </Container>
+      <Router />
     </>
   );
 };
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import { Paper } from "@mui/material";
-import Button from "@mui/material/Button";
+import { Paper, Box, Typography, TextField, Button } from "@mui/material";
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import toast from "react-hot-toast";
+import { Navigate } from "react-router-dom";
 
-import styles from "./Login.module.scss";
 import { fetchAuth } from "../../redux/slice/authActionCreator";
 import { selectIsAuth } from "../../redux/slice/auth";
-import { Navigate } from "react-router-dom";
+
+import styles from "./Login.module.scss";
 
 export const Login = () => {
 
@@ -41,32 +39,35 @@ export const Login = () => {
   if (isAuth) {
     return <Navigate to='/' />
   }
+
   return (
-    <Paper classes={{ root: styles.root }}>
-      <Typography classes={{ root: styles.title }} variant="h5">
-        Вход в аккаунт
-      </Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <TextField
-          className={styles.field}
-          label="E-Mail"
-          type='email'
-          error={Boolean(errors.email?.message)}
-          helperText={errors.email?.message}
-          {...register('email', { require: 'Укажіть почту' })}
-          fullWidth
-        />
-        <TextField
-          className={styles.field}
-          label="Пароль"
-          helperText={errors.password?.message}
-          {...register('password', { require: 'Укажіть пароль' })}
-          fullWidth
-        />
-        <Button type="submit" size="large" variant="contained" fullWidth>
-          Войти
-        </Button>
-      </form>
-    </Paper>
+    <Box style={{ height: 'calc(100vh - 50px)', display: 'flex', alignItems: 'center' }}>
+      <Paper classes={{ root: styles.paper }}>
+        <Typography classes={{ root: styles.title }} variant="h5">
+          Вход в аккаунт
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            className={styles.field}
+            label="E-Mail"
+            type='email'
+            error={Boolean(errors.email?.message)}
+            helperText={errors.email?.message}
+            {...register('email', { require: 'Укажіть почту' })}
+            fullWidth
+          />
+          <TextField
+            className={styles.field}
+            label="Пароль"
+            helperText={errors.password?.message}
+            {...register('password', { require: 'Укажіть пароль' })}
+            fullWidth
+          />
+          <Button type="submit" size="large" variant="contained" fullWidth>
+            Войти
+          </Button>
+        </form>
+      </Paper>
+    </Box>
   );
 };
